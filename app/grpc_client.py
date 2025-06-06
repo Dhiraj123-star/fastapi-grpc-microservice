85f2528e171f685127b0af9cdffa6fa4a6c35d4a
Dhiraj Kumar <gdhiraj499@gmail.com>
2025-06-06 12:31:21 +0530

    Add GitHub Actions workflow for CI/CD Docker image build and push

--- app/grpc_client.py
+++ app/grpc_client.py
@@ -3,7 +3,7 @@ from app.proto import user_pb2, user_pb2_grpc
 
 def get_user_by_id(user_id: int):
     print(f"Sending gRPC request for user_id: {user_id}")
-    with grpc.insecure_channel('localhost:50051') as channel:
+    with grpc.insecure_channel('grpc-server:50051') as channel:
         stub = user_pb2_grpc.UserServiceStub(channel)
         request = user_pb2.UserRequest(id=user_id)
         response = stub.GetUser(request)
@@ -11,7 +11,7 @@ def get_user_by_id(user_id: int):
 
 
 def add_fake_user():
-    with grpc.insecure_channel('localhost:50051') as channel:
+    with grpc.insecure_channel('grpc-server:50051') as channel:
         stub = user_pb2_grpc.UserServiceStub(channel)
         request = user_pb2.Empty()
         response = stub.AddFakeUser(request)
